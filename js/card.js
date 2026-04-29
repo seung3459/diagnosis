1e251e7caa4628c7613d8d2782ee32c8ce3e182c
Ruci <69108157+seung3459@users.noreply.github.com>
2026-04-29 10:14:15 +0900

    Change installation date input type to month

--- js/card.js
+++ js/card.js
@@ -108,7 +108,7 @@ function renderCard(type, id){
         <div class="grid grid-2">
           <div class="field"><label>제조사</label><div class="input-wrap"><input type="text" id="${type}_${id}_np_maker" placeholder="예: 캐리어"></div></div>
           <div class="field"><label>모델명</label><div class="input-wrap"><input type="text" id="${type}_${id}_np_model" placeholder="예: 19XR"></div></div>
-          <div class="field"><label>설치일자</label><div class="input-wrap"><input type="date" id="${type}_${id}_np_date"></div></div>
+          <div class="field"><label>설치일자</label><div class="input-wrap"><input type="month" id="${type}_${id}_np_date"></div></div>
           <div class="field"><label>냉방능력</label><div class="input-wrap"><input type="number" id="${type}_${id}_np_cooling" placeholder="0"><span class="unit">RT</span></div></div>
           <div class="field"><label>정격 유량</label><div class="input-wrap"><input type="number" id="${type}_${id}_np_flow" placeholder="0"><span class="unit">LPM</span></div></div>
           <div class="field"><label>정격 동력</label><div class="input-wrap"><input type="number" id="${type}_${id}_np_power" placeholder="0"><span class="unit">kW</span></div></div>
@@ -136,7 +136,7 @@ function renderCard(type, id){
     </div>
   `;
 
-  // ⭐ 명판사항 ↔ 운전 측정값 2열 배치 (coldSource만 적용)
+  // 측정값 섹션 HTML (재사용)
   const measureBlock = `
     <div class="section-title">📊 운전 측정값</div>
     <div class="cs-sub-card">
@@ -145,6 +145,7 @@ function renderCard(type, id){
     </div>
   `;
 
+  // coldSource(명판 있음)는 명판/측정값 2열, 그 외는 기존대로
   const bodyMain = (type === 'coldSource')
     ? `
       <div class="np-meas-grid">
